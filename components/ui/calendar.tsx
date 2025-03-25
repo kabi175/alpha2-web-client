fc1807a2f60db35163ab9629d4fef349e863de48
kabilan.muthusamy <kabilan.muthusamy@zohocorp.com>
2025-03-26 02:15:23 +0530

    feat: update Calendar component to support additional Chevron icons and upgrade react-day-picker to version 9.6.3

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,11 +1,16 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import * as React from "react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronUp,
+  ChevronDown,
+} from "lucide-react";
+import { DayPicker } from "react-day-picker";
 
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
 function Calendar({
   className,
@@ -60,16 +65,31 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        Chevron: ({ orientation, className, ...props }) => {
+          switch (orientation) {
+            case "left":
+              return (
+                <ChevronLeft className={cn("size-4", className)} {...props} />
+              );
+            case "down":
+              return (
+                <ChevronDown className={cn("size-4", className)} {...props} />
+              );
+            case "right":
+              return (
+                <ChevronRight className={cn("size-4", className)} {...props} />
+              );
+            case "up":
+            default:
+              return (
+                <ChevronUp className={cn("size-4", className)} {...props} />
+              );
+          }
+        },
       }}
       {...props}
     />
-  )
+  );
 }
 
-export { Calendar }
+export { Calendar };
