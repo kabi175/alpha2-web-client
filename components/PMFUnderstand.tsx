d562f1af5ed51a7e17b77fc8aaab3bef0993bec4
kabilan.muthusamy <kabilan.muthusamy@zohocorp.com>
2025-03-31 09:42:14 +0530

    feat: enhance PMSInfoCard component with additional text for improved information delivery

--- components/PMFUnderstand.tsx
+++ components/PMFUnderstand.tsx
@@ -11,9 +11,21 @@ const PMFUnderstand: React.FC = () => {
 
       <div className="w-full overflow-x-auto pb-4 mt-24 max-md:mt-10 custom-scrollbar">
         <div className="flex gap-7 justify-center px-20 max-md:px-5 min-w-min">
-          <PMSInfoCard image="profit.svg" />
-          <PMSInfoCard image="export.svg" />
-          <PMSInfoCard image="research.svg" />
+          <PMSInfoCard
+            image="profit.svg"
+            text1="PMS are SEBI regulated investment products."
+            text2="sit in your demat account with a regulated custodian."
+          />
+          <PMSInfoCard
+            image="export.svg"
+            text1="PMS are led by seasoned experts like Sunil Singhania (ex-Nippon MF)."
+            text2="Leveraging deep experience and analytical prowess"
+          />
+          <PMSInfoCard
+            image="research.svg"
+            text1="PMS maintain a well-researched portfolio of  15-30 stocks."
+            text2="This enables it to aim for superior long-term returns."
+          />
         </div>
       </div>
     </AlphaSection>
--- components/PMSInfoCard.tsx
+++ components/PMSInfoCard.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 
-const PMSInfoCard: React.FC<{ image: string }> = ({ image }) => {
+const PMSInfoCard: React.FC<{
+  image: string;
+  text1: string;
+  text2: string;
+}> = ({ image, text1, text2 }) => {
   return (
     <article className="bg-gradient-to-br from-slate-900 to-[#666666] via-gray-800 overflow-hidden relative p-10 rounded-3xl text-[white] w-[265px] max-md:mx-auto max-md:my-0 max-md:w-full max-md:max-w-[265px] max-sm:p-6 max-sm:h-auto max-sm:min-h-[400px]">
       <header className="mb-20">
@@ -8,12 +12,8 @@ const PMSInfoCard: React.FC<{ image: string }> = ({ image }) => {
         <Image src={image} alt={"profit image"} width={64} height={64} />
       </header>
       <section className="flex flex-col gap-11">
-        <h2 className="text-xl font-light leading-6 max-sm:text-lg">
-          PMS are SEBI regulated investment products.
-        </h2>
-        <h2 className="text-xl font-light leading-6 max-sm:text-lg">
-          PMS Stocks sat in your demat account with a regulated custodian.
-        </h2>
+        <h2 className="text-xl font-light leading-6 max-sm:text-lg">{text1}</h2>
+        <h2 className="text-xl font-light leading-6 max-sm:text-lg">{text2}</h2>
       </section>
     </article>
   );
