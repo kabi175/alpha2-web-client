d7dc435948ec2369711f168a785d5d5da5b67a5e
kabilan.muthusamy <kabilan.muthusamy@zohocorp.com>
2025-04-10 10:30:57 +0530

    fix: update Footer component layout for improved responsiveness

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -35,7 +35,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="w-full backdrop-blur-[[8.433px]] bg-[#282A34] bg-opacity-20">
       <div className="flex flex-col gap-20 items-start px-28 py-16 max-md:px-16 max-sm:px-5">
-        <div className="flex gap-8 items-center max-md:flex-col max-md:items-start">
+        <div className="flex flex-wrap gap-8 items-center max-md:flex-col max-md:items-start">
           <FooterLogo />
           <div className="flex gap-8 items-start max-md:flex-wrap max-sm:flex-col">
             <FooterNavSection {...navSections.about} />
