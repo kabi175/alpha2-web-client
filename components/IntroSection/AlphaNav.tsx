e6b67cc57ca06105d50fce23442ad35a5939a5ab
kabilan.muthusamy <kabilan.muthusamy@zohocorp.com>
2025-04-11 00:30:05 +0530

    fix: update AlphaNav component for improved layout and consistent text sizing

--- components/IntroSection/AlphaNav.tsx
+++ components/IntroSection/AlphaNav.tsx
@@ -6,38 +6,35 @@ import { redirect } from "next/navigation";
 
 export default function AlphaNav() {
   return (
-    <nav className="flex gap-10 items-center px-8 py-1.5 bg-[color:var(--nav-bg)] rounded-[290px] w-fit max-sm:hidden">
-      <div className="px-4 py-2 bg-[color:var(--nav-active)] rounded-[40px]">
-        <i className="ti ti-home" aria-hidden="true" />
-        <Image
-          onClick={() => redirect("/")}
-          src="/nav-home.svg"
-          width={24}
-          height={24}
-          alt="Home"
-        />
-      </div>
+    <nav className="flex gap-10 items-center px-8 py-2 bg-[color:var(--nav-bg)] rounded-[290px] w-fit max-sm:hidden text-nowrap">
+      <Image
+        onClick={() => redirect("/")}
+        src="/nav-home.svg"
+        width={24}
+        height={24}
+        alt="Home"
+      />
       <Link
         href="/calculator"
-        className="text-base text-white no-underline duration-[0.3s] transition-[color] hover:text-[color:var(--primary-blue)]"
+        className="text-xs lg:text-base text-white no-underline duration-[0.3s] transition-[color] hover:text-[color:var(--primary-blue)]"
       >
         Compare PMS
       </Link>
       <Link
         href="/explore"
-        className="text-base text-white no-underline duration-[0.3s] transition-[color] hover:text-[color:var(--primary-blue)]"
+        className="text-xs lg:text-base text-white no-underline duration-[0.3s] transition-[color] hover:text-[color:var(--primary-blue)]"
       >
         Explore PMS
       </Link>
       <Link
         href="/#faqs"
-        className="text-base text-white no-underline duration-[0.3s] transition-[color] hover:text-[color:var(--primary-blue)]"
+        className="text-xs lg:text-base text-white no-underline duration-[0.3s] transition-[color] hover:text-[color:var(--primary-blue)]"
       >
         FAQs
       </Link>
       <Link
         href="/about-us"
-        className="text-base text-white no-underline duration-[0.3s] transition-[color] hover:text-[color:var(--primary-blue)]"
+        className="text-xs lg:text-base text-white no-underline duration-[0.3s] transition-[color] hover:text-[color:var(--primary-blue)]"
       >
         About Us
       </Link>
