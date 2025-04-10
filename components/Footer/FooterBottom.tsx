5e07ad6e90459d3ac057fdad7e4f72eebe8ee685
kabilan.muthusamy <kabilan.muthusamy@zohocorp.com>
2025-04-11 00:36:08 +0530

    fix: adjust layout structure in FooterBottom component for improved readability

--- components/Footer/FooterBottom.tsx
+++ components/Footer/FooterBottom.tsx
@@ -3,13 +3,15 @@ import * as React from "react";
 
 export const FooterBottom: React.FC = () => {
   return (
-    <div className="w-full flex justify-between items-center max-md:px-16 max-sm:flex-col max-sm:gap-2.5 max-sm:items-start max-sm:px-5">
-      <p className="text-sm text-zinc-400">
-        Copyright 2025 of AlphaSqr. All Rights Reserved
-      </p>
-      <p className="text-sm text-zinc-400">
-        APMI regn: APRN04981 valid until 17/03/2028
-      </p>
+    <div className="w-full flex justify-between items-start max-md:px-16 max-sm:flex-col max-sm:gap-2.5 max-sm:items-start max-sm:px-5">
+      <div>
+        <p className="text-sm text-zinc-400 pb-2">
+          Copyright 2025 of AlphaSqr. All Rights Reserved
+        </p>
+        <p className="text-sm text-zinc-400">
+          APMI regn: APRN04981 valid until 17/03/2028
+        </p>
+      </div>
 
       <div className="flex gap-2 items-center">
         <p className="text-sm text-zinc-400">
