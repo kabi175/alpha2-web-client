2809b47c377da513bd4c89ecb393c018ce275b6d
kabi175 <kabilan.mee@gmail.com>
2025-04-15 23:15:02 +0530

    resolve build issue

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -2,13 +2,15 @@
 
 import AlphaSection from "@/components/AlphaSection";
 import ReturnsDashboard from "@/app/calculator/ReturnsDashboard";
-import React from "react";
+import React, { Suspense } from "react";
 
 export default function Calculator() {
   return (
     <div>
       <AlphaSection isTop>
-        <ReturnsDashboard />
+        <Suspense>
+          <ReturnsDashboard />
+        </Suspense>
       </AlphaSection>
     </div>
   );
