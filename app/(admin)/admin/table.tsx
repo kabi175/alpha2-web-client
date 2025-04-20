c331c595c74f2efc025980f2ba6de99d0b55d1e2
kabi175 <kabilan.mee@gmail.com>
2025-04-20 09:48:54 +0530

    feat: implement pagination component and integrate it into the Fund House table

--- app/(admin)/admin/table.tsx
+++ app/(admin)/admin/table.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table";
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell, TableFooter } from "@/components/ui/table";
 import { Pen } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
@@ -8,38 +9,62 @@ export function TableDemo() {
 
     const router = useRouter()
 
-    const [data, setData] = useState<FundHouseData[]>([]);
+    const [records, setRecords] = useState<FundHouseData[]>([]);
+    const [page, setPage] = useState(1);
+    const perPage = 10;
+
 
     useEffect(() => {
         const fetchData = async () => {
-            const data = await fetchFundHouseData({ page: 1, per_page: 10, cb: () => router.push("/login") });
-            setData(data)
+            const data = await fetchFundHouseData({ page: page, per_page: perPage, cb: () => router.push("/login") });
+            setRecords(data)
         }
 
         fetchData();
-    }, [])
+    }, [page])
 
     return (
-        <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead> Name </TableHead>
-                    <TableHead> Dispaly Name </TableHead>
-                    <TableHead>  </TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {data.map((record) => (
-                    <TableRow key={record.id}>
-                        <TableCell className="font-medium">{record.name}</TableCell>
-                        <TableCell className="font-medium">{record.display_name}</TableCell>
-                        <TableCell onClick={() => router.push(`/admin/fund-house/${record.id}`)} >
-                            <Pen /></TableCell>
+        <>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead> Name </TableHead>
+                        <TableHead> Dispaly Name </TableHead>
+                        <TableHead>  </TableHead>
                     </TableRow>
-                ))
-                }
-            </TableBody >
-        </Table >
+                </TableHeader>
+                <TableBody>
+                    {records.map((record) => (
+                        <TableRow key={record.id}>
+                            <TableCell className="font-medium">{record.name}</TableCell>
+                            <TableCell className="font-medium">{record.display_name}</TableCell>
+                            <TableCell onClick={() => router.push(`/admin/fund-house/${record.id}`)} >
+                                <Pen /></TableCell>
+                        </TableRow>
+                    ))
+                    }
+                </TableBody >
+            </Table >
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem onClick={() => setPage(p => {
+                        if (p > 1) {
+                            return p - 1
+                        }
+                        return p
+                    })} >
+                        <PaginationPrevious href="#" />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#" isActive> {page}</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem onClick={() => setPage(p => p + 1)}>
+                        <PaginationNext href="#" hidden={records.length < perPage} />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
+        </>
+
     )
 }
 
