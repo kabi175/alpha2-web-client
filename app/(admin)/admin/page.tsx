01ea957b7e40b62c0a2aecced7a69e995c36c5e2
kabi175 <kabilan.mee@gmail.com>
2025-05-26 01:26:25 +0530

    resolve build issue

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -3,54 +3,59 @@
 import { TableDemo } from "./table";
 import { Suspense } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useRouter } from "next/navigation";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Page() {
+  return (
+    <Suspense>
+      <Comp />
+    </Suspense>
+  );
+}
+
+function Comp() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const page = Number.parseInt(searchParams.get("page") || "1");
   return (
-    <Suspense>
-      <div className="w-full flex justify-center">
-        <div className="w-full max-w-7xl px-4 py-8">
-          <h1 className="text-2xl font-bold">Fund House</h1>
-          <Tabs
-            defaultValue={
-              searchParams.get("unverified") === "true"
-                ? "unverified"
-                : "all-funds"
-            }
-            className="mt-4"
-          >
-            <TabsList>
-              <TabsTrigger
-                value="all-funds"
-                onClick={() => {
-                  router.push(`/admin?unverified=false&page=${page}`);
-                }}
-              >
-                {" "}
-                All Funds
-              </TabsTrigger>
-              <TabsTrigger
-                value="unverified"
-                onClick={() => {
-                  router.push(`/admin?unverified=true&page=${page}`);
-                }}
-              >
-                Unverified
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="all-funds">
-              <TableDemo />
-            </TabsContent>
-            <TabsContent value="unverified">
-              <TableDemo />
-            </TabsContent>
-          </Tabs>
-        </div>
+    <div className="w-full flex justify-center">
+      <div className="w-full max-w-7xl px-4 py-8">
+        <h1 className="text-2xl font-bold">Fund House</h1>
+        <Tabs
+          defaultValue={
+            searchParams.get("unverified") === "true"
+              ? "unverified"
+              : "all-funds"
+          }
+          className="mt-4"
+        >
+          <TabsList>
+            <TabsTrigger
+              value="all-funds"
+              onClick={() => {
+                router.push(`/admin?unverified=false&page=${page}`);
+              }}
+            >
+              {" "}
+              All Funds
+            </TabsTrigger>
+            <TabsTrigger
+              value="unverified"
+              onClick={() => {
+                router.push(`/admin?unverified=true&page=${page}`);
+              }}
+            >
+              Unverified
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="all-funds">
+            <TableDemo />
+          </TabsContent>
+          <TabsContent value="unverified">
+            <TableDemo />
+          </TabsContent>
+        </Tabs>
       </div>
-    </Suspense>
+    </div>
   );
 }
