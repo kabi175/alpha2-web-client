2e29b1b3b2d6df0409118d24eebd14c24b1b7b31
kabi175 <kabilan.mee@gmail.com>
2025-07-20 18:22:28 +0530

    feat: enhance StrategySection with dynamic tabs and improved steps management

--- app/(public)/mf-pms/strategy-section.tsx
+++ app/(public)/mf-pms/strategy-section.tsx
@@ -1,12 +1,31 @@
+"use client";
 import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Section from "./section";
+import { useState } from "react";
+
+interface Strategy {
+  titile: string;
+  content: string;
+  steps: string[];
+}
 
 export default function StrategySection() {
   const steps = [
     "Identification of broad trends using parameters",
-    "Parameters evaluated are Nifty Spot, BSE 500 PE, BSE 500 EPS Growth, my-Cap to GDP, USA Vix, India Vix, US & India T-Bill spread, Gold Spot, Crude Spot, Fed & RBI stance, MSCI Index Weight",
+    "We evaluate a comprehensive set of macro, market, and sentiment indicators across geographies to assess positioning and timing.",
     "Interplay between these factors provide directional sense of M3 money supply, capital allocation to safe assets, and risk tolerance of global investors",
+  ];
+
+  const steps2 = [
+    "Identification of sectors attracting inflows",
+    "Leadership selection amongst chosen sectors",
+    "Portfolio construction by eliminating MFs with high volatility and governance issues",
+  ];
+
+  const steps3 = [
+    "Identify Mutual Funds which are consistent both from a qualitative and quantitative metrics",
+    "Timely rebalancing basis market cycles",
   ];
 
   const strategies = [
@@ -14,18 +33,23 @@ export default function StrategySection() {
       titile: "Pillar 1",
       content:
         "Proprietary model of identification of market direction using eleven different parameters",
+      steps: steps,
     },
     {
       titile: "Pillar 2",
       content:
         "Proprietary model of identification of market direction using eleven different parameters",
+      steps: steps2,
     },
     {
       titile: "Pillar 3",
       content:
         "Proprietary model of identification of market direction using eleven different parameters",
+      steps: steps3,
     },
   ];
+
+  const [strategy, setStrategy] = useState<Strategy>(strategies[0]);
 
   return (
     <Section>
@@ -44,6 +68,11 @@ export default function StrategySection() {
             <Tabs
               defaultValue={strategies[0].titile}
               className="h-full flex p-12 justify-center items-center"
+              onValueChange={(s) =>
+                setStrategy(
+                  strategies.find((st) => st.titile === s) || strategies[0]
+                )
+              }
             >
               {strategies.map((s) => (
                 <TabsContent
@@ -69,7 +98,7 @@ export default function StrategySection() {
             </Tabs>
           </div>
           <div className="md:w-1/2 bg-[#1B1B1B] p-12 rounded-r-md">
-            {steps.map((step, index) => (
+            {strategy?.steps.map((step, index) => (
               <div key={index} className="flex flex-col">
                 {index != 0 && <Separator className="my-5" />}
                 <div className="flex flex-col">
