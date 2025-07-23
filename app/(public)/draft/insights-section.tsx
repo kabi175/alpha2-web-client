1602399c68ebb8bf99635d4a4b1ff8f8e3ba1143
kabi175 <kabilan.mee@gmail.com>
2025-07-24 00:10:28 +0530

    feat: refactor InsightsSection to improve chart rendering and layout

--- app/(public)/draft/insights-section.tsx
+++ app/(public)/draft/insights-section.tsx
@@ -7,14 +7,6 @@ import { ChartConfig, ChartContainer } from "@/components/ui/chart";
 import { XAxis, Bar, BarChart, LabelList, Legend } from "recharts";
 import { Payload } from "recharts/types/component/DefaultLegendContent";
 
-const chartData = [
-  { month: undefined, nifty: undefined, alpha2: undefined },
-  { month: undefined, nifty: undefined, alpha2: undefined },
-  { month: "Year 1", nifty: 1.05, alpha2: 1.1 },
-  { month: "Year 3", nifty: 1.37, alpha2: 1.73 },
-  { month: "Year 5", nifty: 2.82, alpha2: 4.58 },
-];
-
 const chartConfig = {
   nifty: {
     label: "Nifty 50",
@@ -73,89 +65,151 @@ export default function InsightsSection() {
       </SubHeader>
 
       <div className="flex justify-start items-center gap-12 flex-wrap p-10 mt-10 rounded-2xl bg-[#141414]">
-        <div>
-          <Card className="bg-white w-[640px] h-[380px]">
-            <CardContent>
-              <div className="text-black">
-                <div className="flex justify-between">
-                  <p className="font-bold text-2xl">
-                    Financial <br />
-                    Outcome
-                  </p>
-
-                  <Image
-                    src="/logo-black.svg"
-                    alt="logo"
-                    width={56}
-                    height={26}
-                  />
-                </div>
-                <div className="flex justify-between gap-8 pt-4 text-sm">
-                  <p>
-                    Alphasqr recommended PMS delivered
-                    <br /> 80% higher corpus in 5 years
-                  </p>
-
-                  <p>Returns on ₹1 Cr investment</p>
-                </div>
-              </div>
-
-              <ChartContainer config={chartConfig} className="relative">
-                <BarChart data={chartData} className="top-[-100px]">
-                  <XAxis
-                    dataKey="month"
-                    tickLine={false}
-                    tickMargin={10}
-                    lightingColor={"#D9D9D9"}
-                  />
-
-                  <Legend
-                    verticalAlign="bottom"
-                    align="left"
-                    content={renderLegend}
-                  />
-                  {/* <ChartTooltip
-                    cursor={false}
-                    content={<ChartTooltipContent indicator="dashed" />}
-                  /> */}
-                  <Bar dataKey="nifty" fill="#D9D9D9" radius={4}>
-                    <LabelList
-                      position="top"
-                      offset={12}
-                      fontSize={14}
-                      fill="black"
-                      fontWeight={500}
-                    />
-                  </Bar>
-                  <Bar dataKey="alpha2" fill="#111111" radius={4}>
-                    <LabelList
-                      position="top"
-                      offset={12}
-                      fontSize={14}
-                      fill="black"
-                      fontWeight={500}
-                    />
-                  </Bar>
-                </BarChart>
-              </ChartContainer>
-            </CardContent>
-          </Card>
-          <div className="flex gap-2 pt-4 leading-none font-medium">
-            Outperforming the Market with 80% Higher Corpus in 5 Years
-          </div>
-        </div>
-
-        <div>
-          <Image
-            className="pb-4"
-            src="/landing-page/image.png"
-            alt="Insights 1"
-            width={640}
-            height={360}
-          />
-          MF-PMS Strategies Delivered Consistent Outperformance
-        </div>
+        <FinancialOutcome />
+        <PMSCard />
       </div>
     </Section>
   );
 }
+
+const FinancialOutcome = () => {
+  const chartData = [
+    { month: undefined, nifty: undefined, alpha2: undefined },
+    { month: undefined, nifty: undefined, alpha2: undefined },
+    { month: "Year 1", nifty: 1.05, alpha2: 1.1 },
+    { month: "Year 3", nifty: 1.37, alpha2: 1.73 },
+    { month: "Year 5", nifty: 2.82, alpha2: 4.58 },
+  ];
+
+  return (
+    <div>
+      <Card className="bg-white w-[640px] h-[380px]">
+        <CardContent>
+          <div className="text-black">
+            <div className="flex justify-between">
+              <p className="font-bold text-2xl">
+                Financial <br />
+                Outcome
+              </p>
+
+              <Image src="/logo-black.svg" alt="logo" width={56} height={26} />
+            </div>
+            <div className="flex justify-between gap-8 pt-4 text-sm">
+              <p>
+                Alphasqr recommended PMS delivered
+                <br /> 80% higher corpus in 5 years
+              </p>
+
+              <p>Returns on ₹1 Cr investment</p>
+            </div>
+          </div>
+
+          <ChartContainer config={chartConfig} className="relative">
+            <BarChart data={chartData} className="top-[-100px]">
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                tickMargin={10}
+                lightingColor={"#D9D9D9"}
+              />
+
+              <Legend
+                verticalAlign="bottom"
+                align="left"
+                content={renderLegend}
+              />
+              {/* <ChartTooltip
+                    cursor={false}
+                    content={<ChartTooltipContent indicator="dashed" />}
+                  /> */}
+              <Bar dataKey="nifty" fill="#D9D9D9" radius={4}>
+                <LabelList
+                  position="top"
+                  offset={12}
+                  fontSize={14}
+                  fill="black"
+                  fontWeight={500}
+                />
+              </Bar>
+              <Bar dataKey="alpha2" fill="#111111" radius={4}>
+                <LabelList
+                  position="top"
+                  offset={12}
+                  fontSize={14}
+                  fill="black"
+                  fontWeight={500}
+                />
+              </Bar>
+            </BarChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+      <div className="flex gap-2 pt-4 leading-none font-medium">
+        Outperforming the Market with 80% Higher Corpus in 5 Years
+      </div>
+    </div>
+  );
+};
+
+const PMSCard = () => {
+  const chartData = [
+    { month: undefined, nifty: undefined, alpha2: undefined },
+    { month: undefined, nifty: undefined, alpha2: undefined },
+    { month: "Year 1", nifty: 4.6, alpha2: 38.8 },
+    { month: "Year 3", nifty: 23.1, alpha2: 45.1 },
+    { month: "Year 5", nifty: 11.8, alpha2: 23.3 },
+  ];
+
+  return (
+    <div>
+      <Card className="bg-[#0076FF] w-[640px] h-[380px]">
+        <CardContent>
+          <div className="">
+            <div className="flex gap-2 items-center">
+              <p className="font-bold text-2xl">10% +</p>
+              <p className="font-normal text-md"> higher </p>
+            </div>
+            <p className="text-sm">CAGR than Index</p>
+          </div>
+
+          <ChartContainer config={chartConfig} className="relative">
+            <BarChart data={chartData} className="top-[-50px]">
+              <XAxis dataKey="month" tickLine={false} tickMargin={10} />
+
+              <Legend verticalAlign="bottom" align="left" color="white" />
+
+              {/* <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent indicator="line" />}
+              /> */}
+
+              <Bar dataKey="nifty" fill="#5D9CE7E5" radius={4}>
+                <LabelList
+                  position="top"
+                  offset={12}
+                  fontSize={14}
+                  fill="white"
+                  fontWeight={500}
+                  formatter={(value: string) => `${value}%`}
+                />
+              </Bar>
+              <Bar dataKey="alpha2" fill="#FFFFFF" radius={4}>
+                <LabelList
+                  position="top"
+                  offset={12}
+                  fontSize={14}
+                  fill="white"
+                  fontWeight={500}
+                  formatter={(value: string) => `${value}%`}
+                />
+              </Bar>
+            </BarChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+      <div className="flex gap-2 pt-4 leading-none font-medium">
+        MF-PMS Strategies Delivered Consistent Outperformance
+      </div>
+    </div>
+  );
+};
