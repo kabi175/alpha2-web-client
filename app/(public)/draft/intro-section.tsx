b463ac0dda697a1afb11383bf93f267eb652a0ee
kabi175 <kabilan.mee@gmail.com>
2025-08-17 10:02:58 +0530

    Add new SVG icons for landing page: chat bubble, money bag, and tax planning
    - Added chat-bubble.svg for chat functionality representation.
    - Added money-bag.svg to symbolize financial aspects.
    - Added tax-planning.svg to illustrate tax-related services.

--- app/(public)/draft/intro-section.tsx
+++ app/(public)/draft/intro-section.tsx
@@ -1,26 +1,27 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { motion, useAnimation } from "framer-motion";
 import Image from "next/image";
-import { useEffect } from "react";
 import Section from "./section";
 
 const cardItems = [
-  { icon: "arrow.svg", title: "Disciplined Process" },
-  { icon: "diaond.svg", title: "Integrity in Every Solution" },
-  { icon: "rect.svg", title: "Transparent Communication" },
-  { icon: "bulb.svg", title: "Research-Backed Decisions" },
+  { icon: "money-bag.svg", title: "Wealth Management" },
+  { icon: "tax-planning.svg", title: "Tax Planning" },
+  { icon: "bulb.svg", title: "Retirement Solutions" },
+  {
+    icon: "chat-bubble.svg",
+    title: "Financial Counselling",
+  },
 ];
 
 export default function IntroSection() {
   return (
     <div>
       <Section>
-        <div className="w-full h-[600px]">
+        <div className="w-full">
           <div className="text-5xl">
             Invest with Intent.
-            <div className="flex">
-              Secure Your Financial
+            <div className="">
+              <p> Secure Your Financial </p>
               <Image
                 className="p-2"
                 src="/landing-page/currency.png"
@@ -28,11 +29,11 @@ export default function IntroSection() {
                 width={114}
                 height={50}
               />
-              Future.
+              <p> Future. </p>
             </div>
           </div>
 
-          <div className="flex justify-between">
+          <div className="flex flex-col xl:flex-row justify-between">
             <div className="text-[#8E8E8E]">
               Every decision at AlphaSqr is backed by purpose and data—so you
               can
@@ -42,70 +43,45 @@ export default function IntroSection() {
             <Button> Start Investing </Button>
           </div>
 
-          <div className="flex justify-between gap-4 pt-8">
+          <div className="flex flex-col md:flex-row items-center justify-between gap-4 pt-8">
             {cardItems.map((item, index) => (
-              <FinCard key={index} title={item.title} icon={item.icon} />
+              <FinCard
+                key={index}
+                title={item.title}
+                icon={item.icon}
+                isDark={index % 2 != 0}
+              />
             ))}
           </div>
         </div>
-        <div className="flex justify-center text-xl">
+
+        {/* <div className="flex justify-center text-xl p-8">
           <p> Trusted by Leaders Across Sectors</p>
-        </div>
+        </div> */}
       </Section>
       {/* <TrustedLeaders /> */}
-      <InfiniteHorizontalScroll />
+      {/* <InfiniteHorizontalScroll /> */}
     </div>
   );
 }
 
-const items = ["qualcomm", "zoho", "nomura", "uber", "amara-raja"];
-
-const ScrollItem = ({ leader, bg }: { leader: string; bg?: string }) => (
+const FinCard = ({
+  title,
+  icon,
+  isDark,
+}: {
+  title: string;
+  icon: string;
+  isDark: boolean;
+}) => (
   <div
-    className={`px-8 text-lg whitespace-nowrap flex  items-center justify-center min-w-[302px] min-h-[162px] ${bg}`}
+    className={`${
+      isDark ? "bg-white" : "bg-[#0076FF]"
+    } w-[300px] h-[300px] flex flex-col justify-between p-12 rounded-2xl`}
   >
-    <Image
-      alt={leader}
-      src={`/trusted-leaders/${leader}.png`}
-      width={174}
-      height={32}
-    />
-  </div>
-);
-
-function InfiniteHorizontalScroll() {
-  const controls = useAnimation();
-  const scrollWidth = items.length * 174; // adjust if items have different widths
-
-  useEffect(() => {
-    controls.start({
-      x: [0, -scrollWidth],
-      transition: {
-        repeat: Infinity,
-        ease: "linear",
-        duration: 15, // adjust speed
-      },
-    });
-  }, [controls, scrollWidth]);
-
-  return (
-    <div className="overflow-hidden w-full h-[165px] rounded relative">
-      <motion.div animate={controls} className="absolute flex">
-        {[...items, ...items].map((item, index) => (
-          <ScrollItem
-            key={index}
-            leader={item}
-            bg={index % 2 == 0 ? "bg-[#151515]" : ""} // alternate background color
-          ></ScrollItem>
-        ))}
-      </motion.div>
-    </div>
-  );
-}
-
-const FinCard = ({ title, icon }: { title: string; icon: string }) => (
-  <div className="bg-[#161616] w-[300px] h-[300px] flex flex-col justify-between p-12 rounded-2xl">
     <Image src={`/landing-page/${icon}`} alt={title} width={41} height={40} />
-    <div className="text-lg">{title}</div>
+    <div className={`font-semibold text-lg ${isDark ? "text-black" : ""}`}>
+      {title}
+    </div>
   </div>
 );
