12277058cecd3ec91ef6e4aafcbe08f0ea0de93c
kabi175 <kabilan.mee@gmail.com>
2025-08-20 23:59:12 +0530

    feat: refactor BuiltBySection to use structured investor data and improve image rendering

--- app/(public)/draft/builtby-section.tsx
+++ app/(public)/draft/builtby-section.tsx
@@ -2,7 +2,18 @@ import Image from "next/image";
 import Section from "./section";
 import { Header, SubHeader } from "./ui";
 
-const investors = ["/ankit.png", "/dheeraj.jpeg"];
+const investors = [
+  {
+    img: "/ankit.png",
+    name: "Ankit Himatsingka",
+    title: "15+ years in finance",
+  },
+  {
+    img: "/dheeraj.jpeg",
+    name: "Dheeraj Reddy",
+    title: "5+ years in global markets",
+  },
+];
 
 export default function BuiltBySection() {
   return (
@@ -16,16 +27,36 @@ export default function BuiltBySection() {
 
       <div className="flex justify-start items-center gap-8 flex-wrap pt-10">
         {investors.map((item, index) => (
-          <Image
-            className="w-[400px] h-[500px]"
-            key={index}
-            src={item}
-            alt={item}
-            width={317}
-            height={360}
-          />
+          <ImageCard key={index} {...item} />
         ))}
       </div>
     </Section>
   );
 }
+
+const ImageCard = ({
+  img,
+  name,
+  title,
+}: {
+  img: string;
+  name: string;
+  title: string;
+}) => {
+  return (
+    <div className="w-[400px] h-[500px] relative">
+      <Image
+        src={img}
+        alt="Ankit"
+        width={317}
+        height={360}
+        className="w-full h-full object-cover"
+      />
+      <div className="absolute w-full h-[60px] backdrop-blur-xs bottom-0 left-0"></div>
+      <div className="absolute bottom-[5px] left-[20px]">
+        <p className="font-bold">{name}</p>
+        <p> {title}</p>
+      </div>
+    </div>
+  );
+};
